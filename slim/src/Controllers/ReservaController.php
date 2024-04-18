681cf0f9d6a396d0bf7e0d560ad51526a95a76ff
FranMariotti <francomariotti2004@gmail.com>
2024-04-18 08:27:59 -0300

    cambio reserva controller

--- slim/src/Controllers/ReservaController.php
+++ slim/src/Controllers/ReservaController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Models\Reserva;
+use App\Models\propiedad;
+use app\Models\Inquilino;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -12,25 +14,31 @@ class ReservaController
     {
         $respuesta = array();
         if (!isset($data['propiedadId']) || empty($data['PropiedadId'])) {
-            $error = 'Error. El campo propiedadId es obligatorio.';
-            $respuesta[] = $error;
+            $error = 'Error. El campo propiedad es obligatorio.';
+        } else {
+            $propiedadId = Propiedad::find($data['propiedadId']);
+            if ($propiedadId == null) {
+                $error = 'Error. la propiedad no existe.';
+                $respuesta[] = $error;
+            }
         }
         if (!isset($data['inquilinoId']) || empty($data['inquilinoId'])) {
-            $reserva = Reserva::find($data['inquilinoId']);
-            if ($reserva == null) {
+            $error = 'Error. El campo inquilino es obligatorio.';
+        } else {
+            $inquilino = Inquilino::find($data['inquilinoId']);
+            if ($inquilino == null) {
                 $error = 'Error. el inquilino no existe.';
                 $respuesta[] = $error;
             }
-            if (isset($data['fechaInicio']) && !empty($data['fechaInicio'])) {
-                $error = 'Error. el campo frvhaInicio es obligatorio.';
-                $respuesta[] = $error;
-            }
+        }
+        if (isset($data['fechaInicio']) && !empty($data['fechaInicio'])) {
+            $error = 'Error. el campo fechaInicio es obligatorio.';
+            $respuesta[] = $error;
         }
         if (!isset($data['cantNoches']) || empty($data['cantNoches'])) {
             $error = 'Error. El campo cantidad de noches es obligatorio.';
             $respuesta[] = $error;
         }
-
         if (count($respuesta) > 0) {
             return $respuesta;
         } else {
