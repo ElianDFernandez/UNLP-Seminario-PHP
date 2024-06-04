f62f0a24929bca7d85f2673abf5c802f428b143c
FranMariotti <francomariotti2004@gmail.com>
2024-06-04 12:31:59 -0300

    agrego el use state a reservas

--- inmobiliaria/src/pages/Reservas.jsx
+++ inmobiliaria/src/pages/Reservas.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Boton from "../components/commons/Boton";
+import { useState, useEffect } from "react";
+import { Apiget } from "../utils/api";
+
 const Reservas = () => {
   const crearReserva = () => {
     console.log("crearReserva");
   };
+  const [Reservas, setReservas] = useState([]);
+
+  useEffect(() => {
+    Apiget("http://localhost:80/reservas").then((data) => {
+      console.log(data);
+      if (Array.isArray(data)) {
+        setReservas(data);
+      } else {
+        console.error("Data is not an array:", data);
+      }
+    });
+  }, []);
+
   return (
     <div>
       <h1>Reservas</h1>
-      <h3>aca irian las Reservas</h3>
+      {Reservas.map((res, index) => (
+        <div key={index}>
+          <p>{res.fecha_inicio}</p>
+        </div>
+      ))}
       <Boton texto="nueva Reserva" onClick={crearReserva} />
     </div>
   );
